c992cca31be6d1cf946083f3999862121e63f6d4
Mark L. Chang <mark.chang@gmail.com>
2011-02-25 22:33:46 -0500

    new!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NarwhalRails::Application.config.secret_token = 'b002e85e8af866f1c1132243bed5cd4dbf59e98b61a755fb2e0e79c9be93fc4e5f7d1cff8e813ecb36fbd1f53f92978a1901b9b63e6cea518073690b23241937'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+NarwhalRails::Application.config.session_store :cookie_store, :key => '_narwhal-rails_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# NarwhalRails::Application.config.session_store :active_record_store
