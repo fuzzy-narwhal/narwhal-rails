09f58fd667e30d68f929e8380e196bd4f70cd9d3
Mark L. Chang <mark.chang@gmail.com>
2011-02-26 01:23:30 -0500

    seeding from facebook!

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,28 @@
 class Post < ActiveRecord::Base
 end
+
+# == Schema Information
+#
+# Table name: posts
+#
+#  id               :integer         not null, primary key
+#  likes            :integer
+#  post_id          :string(255)
+#  from             :string(255)
+#  to               :string(255)
+#  message          :string(255)
+#  picture          :string(255)
+#  link_url         :string(255)
+#  link_name        :string(255)
+#  link_caption     :string(255)
+#  link_description :string(255)
+#  movie_url        :string(255)
+#  icon             :string(255)
+#  attribution      :string(255)
+#  created_time     :datetime
+#  updated_time     :datetime
+#  comments         :integer
+#  created_at       :datetime
+#  updated_at       :datetime
+#
+
