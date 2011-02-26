4d3503f0c2fb7ab8b98a8ac589babaa296613ca9
Mark L. Chang <mark.chang@gmail.com>
2011-02-26 01:54:50 -0500

    first crawler done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110226044158) do
+ActiveRecord::Schema.define(:version => 20110226064137) do
+
+  create_table "pages", :force => true do |t|
+    t.string   "page_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "posts", :force => true do |t|
     t.integer  "likes"
