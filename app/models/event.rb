1718b9745464b038c4e954cc87d7b8777475826c
marstall <chris@tourfilter.com>
2011-02-27 01:59:21 -0500

    fix

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,17 @@
 class Event < ActiveRecord::Base
 
-def Event.current
-  find_by_sql(["select * from events where end_time>? order by start_time asc limit 100",DateTime.now])
+def Event.current(category)
+  if category
+    sql = <<-SQL
+      select events.* from events,pages
+      where events.page_id=pages.page_id
+      and pages.category like "%#{category}%"
+      and end_time>?
+      order by start_time asc limit 100
+    SQL
+  else
+    sql = "select * from events where end_time>? order by start_time asc limit 100"
+  end
+  find_by_sql([sql,DateTime.now])
 end
 end
