13a8559a59f66f9a40a523ea133d03c5778de47c
marstall <chris@tourfilter.com>
2011-02-27 02:17:58 -0500

    filters for events

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,9 +3,11 @@ class Event < ActiveRecord::Base
 def Event.current(category)
   if category
     sql = <<-SQL
-      select events.* from events,pages
+      select events.* from events,pages,categories_pages,categories
       where events.page_id=pages.page_id
-      and pages.category like "%#{category}%"
+      and pages.id = categories_pages.page_id
+      and categories_pages.category_id=categories.id
+      and categories.name like "%#{category}%"
       and end_time>?
       order by start_time asc limit 100
     SQL
@@ -15,3 +17,19 @@ def Event.current(category)
   find_by_sql([sql,DateTime.now])
 end
 end
+=begin
+select events.* from events,pages,categories_pages,categories
+where events.page_id=pages.page_id
+and pages.id = categories_pages.page_id
+and categories_pages.category_id=categories.id
+and categories.name like '%movies%'
+and end_time>'2011-02-27 07:10:35'
+order by start_time asc limit 100
+
+select events.* from events,pages,categories_pages,categories
+where events.page_id=pages.page_id
+and pages.id = categories_pages.page_id
+and categories_pages.category_id=categories.id
+and categories.name like "%local%"
+
+=end
